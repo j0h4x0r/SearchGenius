9e6c6045060ab5bb6e36b0b7d862a834f4cc7cd7
j0h4x0r <troyal.lee@gmail.com>
2015-02-27 12:01:09 -0500

    Replace library lxml by xml

--- main.py
+++ main.py
@@ -2,24 +2,27 @@ import requests
 import urllib
 import sys
 import codecs
-from lxml import etree
+import xml.etree.ElementTree as ET
 import nltk
 import itertools
 import math
+import string
+from nltk.corpus import stopwords
 
 rootURL = 'https://api.datamarket.azure.com/Bing/Search/v1/Web'
 # To be substituted
-accKey = 'gQ+gama5GAfLoQmix8AKEn5Nop24Tlu34tRapNPOImI'
+#accKey = 'gQ+gama5GAfLoQmix8AKEn5Nop24Tlu34tRapNPOImI'
 # top N documents we should do calculation on
 N = 10
 # rocchio parameters
-alpha = 1.0
+alpha = 2.0
 beta = 0.75
 gamma = 0.15
 
 def main():
 	queryStr = raw_input('Please input the query word(s), separated by space: ')
 	target = raw_input('Please input the target precision: ')
+	accKey = raw_input('Please input the Bing account key: ')
 	try:
 		target = float(target)
 		if target < 0 or target > 1:
@@ -27,17 +30,22 @@ def main():
 	except ValueError:
 		print 'Illegal precision!'
 		sys.exit()
-
 	# iteration times
 	times = 1
-	data, precision = startSearch(queryStr, times)
+	data, precision = startSearch(queryStr, times, accKey)
 
-	while precision < target:
+	while precision > 0 and precision < target:
 		print 'Still below than the target precision of', target
 		queryStr = adjustQuery(queryStr, data)
 		times += 1
-		data, precision = startSearch(queryStr, times)
-	print 'Target precision reached. Done.'
+		data, precision = startSearch(queryStr, times, accKey)
+	if precision == 0:
+		if data:
+			print 'No relevant documents. Terminated.'
+		else:
+			print 'Less than 10 results received. Terminated.'
+	else:
+		print 'Target precision reached. Done.'
 
 #calculate precison each iteration:
 def calcPrecison(data):
@@ -47,14 +55,7 @@ def calcPrecison(data):
 			rel += 1
 	return float(rel) / len(data)
 
-#pre-process text: str_source is the text needed to be processed; replace all words to char
-def str_replace(str_source,char,*words):
-    str_temp=str_source    
-    for word in words:
-        str_temp=str_temp.replace(word,char)
-    return str_temp	
-
-def startSearch(queryStr, times):
+def startSearch(queryStr, times, accKey):
 	print '========================'
 	print 'Round', times
 	print 'Query:', queryStr
@@ -64,14 +65,24 @@ def startSearch(queryStr, times):
 	# Parse result
 	data = [{} for i in range(N)]
 	# for test only
-	tree = etree.fromstring(result.text.encode('ascii', 'ignore'))
+	tree = ET.fromstring(result.text.encode('ascii', 'ignore'))
 	#tree = etree.fromstring(result.text)
-	metas = tree.findall('.//m:properties', tree.nsmap)
+	# name space
+	nsmap = {
+		'base': 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Web',
+		'm': 'http://schemas.microsoft.com/ado/2007/08/dataservices/metadata',
+		'd': 'http://schemas.microsoft.com/ado/2007/08/dataservices',
+		None: 'http://www.w3.org/2005/Atom'
+	}
+	metas = tree.findall('.//m:properties', nsmap)
+	# if the result < 10, simply terminate
+	if len(metas) < 10:
+		return None, 0
 	#set data dict values
 	for i in range(N):
-		title = metas[i].find('d:Title', tree.nsmap).text
-		description = metas[i].find('d:Description', tree.nsmap).text
-		url = metas[i].find('d:Url', tree.nsmap).text
+		title = metas[i].find('d:Title', nsmap).text
+		description = metas[i].find('d:Description', nsmap).text
+		url = metas[i].find('d:Url', nsmap).text
 		# record data
 		data[i]['url'] = url
 		data[i]['title'] = title
@@ -92,49 +103,25 @@ def startSearch(queryStr, times):
 	print 'Precision:', precision
 	return data, precision
 
-# # Adjust query - key part
-# def adjustQuery(queryStr, data):
-# 	### start calculating TF-IDF scores
-# 	tf={}  
-# 	df={}
-# 	all_doc=[]
-# 	all_word=[]
-# 	tfidf={}
-# 	print 'Indexing results ....'
-# 	for i in xrange(N):
-# 		#tokenize each description, more pre-processing measure to be added...
-# 		tmp_dj=nltk.word_tokenize(data[i]['description'])
-# 		all_doc.append(tmp_dj)
-# 		for word in tmp_dj: 
-# 			word=word.lower() #here we use lower case words for all results
-# 			if word not in tf[i]:
-# 				tf[i][word]=1
-# 			else:
-# 				tf[i][word]=tf[i].get(word)+1
-# 			if word not in all_word:
-# 				all_word.append(word)
-# 	for word in all_word:
-# 		df=0
-# 		for i in xrange(N):
-# 			if word in all_doc[i]:
-# 				df=df+1
-# 		tfidf[word]=(float(tf[i][word])/sum(tf[i].itervalues()))*math.log((float(N)/df),2)
-# 	return rocchio(tfidf,queryStr)
-
 # Adjust query - key part
 def adjustQuery(queryStr, data):
 	qvec, tfidf, word_set = tfidfvec(queryStr, data)
 	new_queryStr = rocchio(qvec, tfidf, data, word_set, queryStr)
 	return new_queryStr
 
+#Calculate tfidf scores
 def tfidfvec(query, data):
 	# extract docs form raw data
 	docs = [item['description'] for item in data]
 	# build a list of tokenized docs
+	stopwds=stopwords.words('english')
 	words_list = []
 	for doc in docs:
-		# you may want to do more pre-processing here...
-		words_list.append([word.lower() for word in nltk.word_tokenize(doc)])
+		s=[]
+		for word in nltk.word_tokenize(doc):
+			if((word not in stopwds) and (word not in string.punctuation)):
+				s.append(word.lower())
+		words_list.append(s)
 	# a list of all words, with duplicates
 	all_words = list(itertools.chain(*words_list))
 	# a list of all words, without duplicates - for vector bases
@@ -158,13 +145,12 @@ def tfidfvec(query, data):
 	qwords = [word.lower() for word in query.split()]
 	# tf vector
 	qvec = [qwords.count(w) for w in word_set]
-	# tf-idf vector
-	qvec = [tf * idf for tf, idf in zip(qvec, idfs)]
 	# normalize
 	nom = math.sqrt(sum(x**2 for x in qvec))
 	qvec = [x / nom for x in qvec]
 	return qvec, tfidf, word_set
 
+# Generate new query words
 def rocchio(qvec, tfidf, data, word_set, old_query):
 	# record relevant & irrelevant count
 	rel_count = sum(1 for item in data if item['rel'])
@@ -194,7 +180,6 @@ def rocchio(qvec, tfidf, data, word_set, old_query):
 			quota -= 1
 	qwords.sort(key = lambda w: new_qvec[word_set.index(w)], reverse = True)
 	queryStr = ' '.join(qwords)
-
 	return queryStr
 
 if __name__ == '__main__': main()
